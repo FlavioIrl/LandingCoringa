41f1c5041b712a4f467fea43cca7acdb6b81645c
Flavio Irala <flavioiralap@gmail.com>
2024-11-03 22:02:32 -0400

    estava dando erro a minificação, para não demorar mais retirei

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
@@ -15,14 +14,12 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-function images() {
-    return gulp.src('./src/imagens/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('./dist/imagens'));
-}
-exports.default = gulp.parallel(scripts, images, styles)
+
+
+exports.default = gulp.parallel(styles, scripts);
 
 exports.watch = function () {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+    gulp.watch('./src/imagens/*', gulp.parallel(imagens));
 }
